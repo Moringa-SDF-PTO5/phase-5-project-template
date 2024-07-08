3b0ea0ccdb3ec37a948deb33e3ae0ed388124159
otsembo <okumu.otsembo@gmail.com>
2024-07-08 06:53:15 +0300

    Update eslint for React CI

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, jest: true},
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -8,11 +13,28 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 12, sourceType: 'module', ecmaFeatures: { jsx: true }},
-  settings: { react: { version: '18.3' } },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: '18.3' },
+  },
   plugins: ['react', 'jest'],
   rules: {
     'react/react-in-jsx-scope': 'off', // Disable rule requiring React in scope for JSX (for React 17+)
     'no-undef': 'off', // Turn off 'no-undef' for jest globals
   },
-}
+  overrides: [
+    {
+      files: ['*.config.js'],
+      env: {
+        node: true, // Node environment for config files
+      },
+      rules: {
+        'no-undef': 'off', // Turn off 'no-undef' for config files
+      },
+    },
+  ],
+};
