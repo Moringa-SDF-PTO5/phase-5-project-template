ef25b73e5c5d87f4f7a6eecfc2cd0a2031bcd331
RosaliaNjoki <njosali0@gmail.com>
2024-07-19 18:43:46 +0300

    appy edited

--- server/app.py
+++ server/app.py
@@ -10,9 +10,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 @app.route('/register', methods=['POST'])
 def register():
