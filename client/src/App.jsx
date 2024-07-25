dcd6ccc6557e4863321e0bd85b0d80556f0f9092
oriri04 <152679239+oriri04@users.noreply.github.com>
2024-07-25 22:05:52 +0300

    Added delivery confirmation page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
-
+// src/App.jsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import OrderConfirmation from './OrderConfirmation';
+import DeliveryConfirmation from './DeliveryConfirmation';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <OrderConfirmation />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<OrderConfirmation />} />
+          <Route path="/delivery-confirmation" element={<DeliveryConfirmation />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
