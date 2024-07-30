0f9bd56a1ad24eb883233571b93b603bd669e7f5
gkiarie <gkaris24@gmail.com>
2024-07-30 22:58:37 +0100

    port change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateAccount from './register';
+import { AppProvider } from './AppContext';
 import Login from './login';
+import HomePage from './HomePage';
+import CategoryPage from './CategoryPage';
 
 function App() {
     return (
+        <AppProvider>
         <Router>
             <div className="app">
                 <Routes>
+                    <Route path="/" element={<HomePage />} />
                     <Route path="/create-account" element={<CreateAccount />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/category/:categoryName" element={<CategoryPage />} />
                 </Routes>
             </div>
         </Router>
+        </AppProvider>
     );
 }
 
